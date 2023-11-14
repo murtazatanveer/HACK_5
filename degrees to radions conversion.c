5bfffad4dac42d8a15d22ef1a7da25e9f829affe
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-11-15 02:30:28 +0500

    changes after compiling degrees program

--- degrees to radions conversion.c
+++ degrees to radions conversion.c
@@ -4,19 +4,18 @@ double degree_to_radians();
 int main(){
 
 double degree;
-printf("Enter value in Degrees : ");
+printf("\nEnter value in Degrees : ");
 scanf("%lf",&degree);
 
-printf("\n\nAfter converting %.2lf degrees we found %.2lf Radians",degree,degree_to_radians(degree));
+printf("\nAfter converting %.1lf degrees we found %.3lf Radians\n",degree,degree_to_radians(degree));
 
-    
 }
 
 double degree_to_radians(double degree){
 
 const float pi = 3.14;
 
-double radians = (pi*degree)/180
+double radians = (pi*degree)/180;
 
 return radians;
 
