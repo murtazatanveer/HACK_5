6bebfed2abaeb426e7268f91bb0eb462b5cc7da5
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-11-16 14:23:48 +0500

    adding function of air_distance

--- Air_Distance.c
+++ Air_Distance.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<math.h>
+
+double air_distance();
 
 int main(){
 
@@ -35,6 +38,25 @@ if( (lat_ori>=-90 && lat_ori<=90) && (lat_des>=-90 && lat_des<=90) && (lon_des>=
 
 }
 
+}
+
+
+double air_distance(double lat_ori , double lat_des , double lon_ori , double lon_des){
+
+const float pi =3.14;
+const int ear_rad = 6371;
+
+//a : lat_ori
+double a = ( pi * lat_ori)/180;
+
+//b : lat_des
+double b = ( pi * lat_des)/180;
+
+//c : lon_ori
+double c = ( pi * lon_ori)/180;
+
+//d : lon_des
+double d = ( pi * lon_des)/180;
 
 
 
