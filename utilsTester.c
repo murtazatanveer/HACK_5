e4b657ce70845802df94e22e9a1900595f5e0f96
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-12-21 02:02:51 +0500

    Test cases for function degreesToRadians();

--- utilsTester.c
+++ utilsTester.c
@@ -6,9 +6,11 @@
 
 int main(){
 
-    
+ printf("\nTest cases for function degreesToRadians(); \n");
 
-printf("\nAfter Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(80));
+printf("\n1st : After Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(80));
+printf("\n2nd : After Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(90));
+printf("\n3rd : After Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(100));
 
  printf("\nAIR DISTANCE : %.2lf",air_distance( 41.9 , 40.8  , -87.6 , -96.7 ));
 
