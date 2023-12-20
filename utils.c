f3234e5c0c7df90e4c08655ddcac4ce9215e5e9b
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-12-21 01:58:06 +0500

    changes according to instructions in hack

--- utils.c
+++ utils.c
@@ -1,14 +1,42 @@
 #include<stdio.h>
+#include<math.h>
 #include "util.h"
 
-double degreesToRadians(double degrees);
 
-int main(){
+double air_distance(double lat_ori , double lat_des , double lon_ori , double lon_des){
 
- printf("\nAIR DISTANCE : %.2lf",air_distance( 41.9 , 40.8  , -87.6 , -96.7 ));
+const float pi =3.14;
+const int ear_rad = 6371;
 
- printf("\nAfter Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(80));
+//a : lat_ori
+double a = ( pi * lat_ori)/180;
 
+//b : lat_des
+double b = ( pi * lat_des)/180;
+
+//c : lon_ori
+double c = ( pi * lon_ori)/180;
+
+//d : lon_des
+double d = ( pi * lon_des)/180;
+
+double distance = acos( (sin(a) * sin(b)) + (cos(a) * cos(b) * cos(d-c)) ) * ear_rad;
+
+return distance;
+
+}
+
+
+
+double degreesToRadians(double degrees){
+
+const float pi=3.14f;
+
+double radians;
+
+radians = (pi*degrees)/180;
+
+return radians;
 
 }
 
---
+++ utilsTester.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include "util.h"
+
+
+
+int main(){
+
+    
+
+printf("\nAfter Converting %.2lf degrees into radians we get : %.2lf",80.00,degreesToRadians(80));
+
+ printf("\nAIR DISTANCE : %.2lf",air_distance( 41.9 , 40.8  , -87.6 , -96.7 ));
+
+ 
+
+
+}
